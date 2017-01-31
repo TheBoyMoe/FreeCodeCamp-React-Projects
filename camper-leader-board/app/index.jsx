c9148f6b2b9544d301095162f2445f1c1977327d
bill fero <billfero@gmail.com>
2017-01-31 18:08:14 +0000

    impl table row and getCamperInfo function which iterates through the user data returning an array of populated table rows

--- camper-leader-board/app/index.jsx
+++ camper-leader-board/app/index.jsx
@@ -80,11 +80,14 @@ let TableHeader = React.createClass({
 
 
 let TableRow = React.createClass({
-	
 	render: function () {
 		return (
 			<tr>
-				<td></td>
+				<td>{this.props.id}</td>
+				<td>{this.props.img}</td>
+				<td>{this.props.username}</td>
+				<td>{this.props.recent}</td>
+				<td>{this.props.alltime}</td>
 			</tr>
 		)
 	}
@@ -92,12 +95,85 @@ let TableRow = React.createClass({
 
 
 let Table = React.createClass({
-	// TODO fetch data, pass to TableRow component
-	render: function () {
-		
+	//getDefaultProps() {
 		// pass in some dummy data
 		// username, img, alltime, recent, lastUpdate
-		let data = [
+		
+		// this.data = [
+		// 	{
+		// 		username: "Manish-Giri",
+		// 		img: "https://avatars.githubusercontent.com/u/11348778?v=3",
+		// 		alltime: 3616,
+		// 		recent: 672,
+		// 		lastUpdate: "2017-01-25T02:06:27.524Z"
+		// 	},
+		// 	{
+		// 		username: "sjames1958gm",
+		// 		img: "https://avatars.githubusercontent.com/u/4639625?v=3",
+		// 		alltime: 5222,
+		// 		recent: 527,
+		// 		lastUpdate: "2017-01-26T18:14:24.778Z"
+		// 	},
+		// 	{
+		// 		username: "JohnPaulWalsh",
+		// 		img: "https://avatars.githubusercontent.com/u/9558634?v=3",
+		// 		alltime: 959,
+		// 		recent: 503,
+		// 		lastUpdate: "2017-01-27T03:58:38.153Z"
+		// 	},
+		// 	{
+		// 		username: "diomed",
+		// 		img: "https://avatars.githubusercontent.com/u/72777?v=3",
+		// 		alltime: 2395,
+		// 		recent: 495,
+		// 		lastUpdate: "2017-01-23T20:00:13.854Z"
+		// 	},
+		// 	{
+		// 		username: "revisualize",
+		// 		img: "https://avatars.githubusercontent.com/u/1588399?v=3",
+		// 		alltime: 2837,
+		// 		recent: 417,
+		// 		lastUpdate: "2017-01-26T18:09:40.213Z"
+		// 	},
+		// 	{
+		// 		username: "anthonygallina1",
+		// 		img: "https://avatars.githubusercontent.com/u/11003055?v=3",
+		// 		alltime: 3061,
+		// 		recent: 407,
+		// 		lastUpdate: "2017-01-23T19:56:17.417Z"
+		// 	},
+		// 	{
+		// 		username: "apottr",
+		// 		img: "https://avatars.githubusercontent.com/u/1924021?v=3",
+		// 		alltime: 1515,
+		// 		recent: 290,
+		// 		lastUpdate: "2017-01-23T20:02:21.721Z"
+		// 	},
+		// 	{
+		// 		username: "Chrono79",
+		// 		img: "https://avatars.githubusercontent.com/u/9571508?v=3",
+		// 		alltime: 2898,
+		// 		recent: 273,
+		// 		lastUpdate: "2017-01-23T19:57:18.008Z"
+		// 	},
+		// 	{
+		// 		username: "Azbo400",
+		// 		img: "https://avatars.githubusercontent.com/u/12162628?v=3",
+		// 		alltime: 490,
+		// 		recent: 267,
+		// 		lastUpdate: "2017-01-27T03:58:59.177Z"
+		// 	},
+		// 	{
+		// 		username: "ArielLeslie",
+		// 		img: "https://avatars.githubusercontent.com/u/6234475?v=3",
+		// 		alltime: 2386,
+		// 		recent: 238,
+		// 		lastUpdate: "2017-01-23T19:55:02.038Z"
+		// 	}
+		// ];
+	//},
+	_getCamperInfo: function () {
+		const data = [
 			{
 				username: "Manish-Giri",
 				img: "https://avatars.githubusercontent.com/u/11348778?v=3",
@@ -169,16 +245,33 @@ let Table = React.createClass({
 				lastUpdate: "2017-01-23T19:55:02.038Z"
 			}
 		];
+		let count = 0;
+		return data.map(function (camper) {
+			return (
+				<TableRow id={camper.lastUpdate} img={camper.img} username={camper.username}
+						  recent={camper.recent} alltime={camper.alltime}/>
+			);
+		})
 		
-		// TODO iterate through, populate row, returning array of rows
-		let rows = this.props.map(function (camper) {
-			
-		});
+	},
+	render: function () {
+		// iterate through, populate row, returning array of rows
+		
+		// let count = 0;
+		// let rows = this.props.data.map(function (camper) {
+		// 	return (
+		// 		<TableRow index={++count} img={camper.img} username={camper.username}
+		// 				recent={camper.recent} alltime={camper.alltime}/>
+		// 	);
+		// });
+		
+		
+		
 		
 		return (
 			<table>
 				<TableHeader/>
-				<tbody>{rows}</tbody>
+				<tbody>{this._getCamperInfo}</tbody>
 			</table>
 		)
 	}
