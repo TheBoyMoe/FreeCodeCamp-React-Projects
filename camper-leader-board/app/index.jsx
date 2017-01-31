e778e6b37813e7f2cf1971551089f698e368f983
bill fero <billfero@gmail.com>
2017-01-31 21:15:59 +0000

    remove dummy data, add axios lib

--- camper-leader-board/app/index.jsx
+++ camper-leader-board/app/index.jsx
@@ -7,7 +7,7 @@
 	[3] https://codepen.io/maci01/pen/YGNAGk *(UI - responsive using media queries - NO bootstrap, sort works)
 	[4] https://codepen.io/Bouncey/pen/OXkgOO (uses table layout & jQuery)
 	[5] https://codepen.io/jbluks/pen/bBoovv
-	[6] https://codepen.io/micakce/pen/pRPQdx?editors=1010 (click to user's profile on FCC)
+	[6] https://codepen.io/micakce/pen/pRPQdx?editors=1010 (uses axios and promises??)**
 	[7] https://codepen.io/AlexOleynyk/pen/gLLyNq
 	[8] https://codepen.io/sufflavus/pen/RazqBE?editors=1010 (check code)
 	[9] https://codepen.io/eddyw/pen/qNZXga (Responsive ui using bootstrap - sort works)
@@ -97,81 +97,10 @@ let TableRow = React.createClass({
 let Table = React.createClass({
 	getInitialState: function () {
 		return {
-			data : [
-				{
-					username: "Manish-Giri",
-					img: "https://avatars.githubusercontent.com/u/11348778?v=3",
-					alltime: 3616,
-					recent: 672,
-					lastUpdate: "2017-01-25T02:06:27.524Z"
-				},
-				{
-					username: "sjames1958gm",
-					img: "https://avatars.githubusercontent.com/u/4639625?v=3",
-					alltime: 5222,
-					recent: 527,
-					lastUpdate: "2017-01-26T18:14:24.778Z"
-				},
-				{
-					username: "JohnPaulWalsh",
-					img: "https://avatars.githubusercontent.com/u/9558634?v=3",
-					alltime: 959,
-					recent: 503,
-					lastUpdate: "2017-01-27T03:58:38.153Z"
-				},
-				{
-					username: "diomed",
-					img: "https://avatars.githubusercontent.com/u/72777?v=3",
-					alltime: 2395,
-					recent: 495,
-					lastUpdate: "2017-01-23T20:00:13.854Z"
-				},
-				{
-					username: "revisualize",
-					img: "https://avatars.githubusercontent.com/u/1588399?v=3",
-					alltime: 2837,
-					recent: 417,
-					lastUpdate: "2017-01-26T18:09:40.213Z"
-				},
-				{
-					username: "anthonygallina1",
-					img: "https://avatars.githubusercontent.com/u/11003055?v=3",
-					alltime: 3061,
-					recent: 407,
-					lastUpdate: "2017-01-23T19:56:17.417Z"
-				},
-				{
-					username: "apottr",
-					img: "https://avatars.githubusercontent.com/u/1924021?v=3",
-					alltime: 1515,
-					recent: 290,
-					lastUpdate: "2017-01-23T20:02:21.721Z"
-				},
-				{
-					username: "Chrono79",
-					img: "https://avatars.githubusercontent.com/u/9571508?v=3",
-					alltime: 2898,
-					recent: 273,
-					lastUpdate: "2017-01-23T19:57:18.008Z"
-				},
-				{
-					username: "Azbo400",
-					img: "https://avatars.githubusercontent.com/u/12162628?v=3",
-					alltime: 490,
-					recent: 267,
-					lastUpdate: "2017-01-27T03:58:59.177Z"
-				},
-				{
-					username: "ArielLeslie",
-					img: "https://avatars.githubusercontent.com/u/6234475?v=3",
-					alltime: 2386,
-					recent: 238,
-					lastUpdate: "2017-01-23T19:55:02.038Z"
-				}
-			]
+			data: []
 		};
 	},
-	getCamperInfo: function () {
+	setCamperInfo: function () {
 		// iterate through, populate row, returning array of rows
 		return this.state.data.map(function (camper, i) {
 			return (
@@ -179,13 +108,30 @@ let Table = React.createClass({
 						  recent={camper.recent} alltime={camper.alltime}/>
 			);
 		})
-		
+	},
+	fetchCamperInfo: function () {
+		// TODO make the api call using axios and js promise
+		jQuery.ajax({
+			method: 'GET',
+			url: '',
+			success: (data) => {
+				this.setState({data}); // update the apps state
+				
+			}
+		})
+	},
+	componentWillMount: function () {
+		// fetch data from server prior to mounting the component
+		this.fetchCamperInfo();
+	},
+	componentDidMount: function () {
+		// down any updates post rendering of component
 	},
 	render: function () {
 		return (
 			<table>
 				<TableHeader/>
-				<tbody>{this.getCamperInfo()}</tbody>
+				<tbody>{this.setCamperInfo()}</tbody>
 			</table>
 		)
 	}
