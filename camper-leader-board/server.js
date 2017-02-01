df9fa8e342bef2ab2d92685b2df72bea6d0af75c
bill fero <billfero@gmail.com>
2017-02-01 18:36:52 +0000

    change express server port to 8000

--- camper-leader-board/server.js
+++ camper-leader-board/server.js
@@ -2,7 +2,7 @@ let express = require('express');
 
 // create web app
 let app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.use(express.static('app')); // folder to server
 app.listen(PORT, function () {
     console.log(`Express Server is listening on port ${PORT}`);
